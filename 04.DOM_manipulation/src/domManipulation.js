64c5588bfa831d52bed9dce4bb27b7f6c3b652c1
Toni <tkukurin@gmail.com>
2016-08-02 22:24:18 +0100

    Added 4th assignment base.

--- 04.DOM_manipulation/src/domManipulation.js
+++ 04.DOM_manipulation/src/domManipulation.js
@@ -1,3 +1,21 @@
 var dom = function(){
   // code goes here
 };
+
+dom.append = function(element, childToBeAppended) {
+}
+
+dom.prepend = function(element, childToBePrepended) {
+}
+
+dom.remove = function(element) {
+}
+
+dom.after = function() {
+}
+
+dom.before = function() {
+}
+
+dom.val = function(element) {
+}
