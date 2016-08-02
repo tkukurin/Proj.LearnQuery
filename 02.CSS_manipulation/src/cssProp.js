3a0ad6abd06beb8b949ce8a320b97dd6d4776884
Toni <tkukurin@gmail.com>
2016-08-02 20:43:08 +0100

    Solved second assignment.

--- 02.CSS_manipulation/src/cssProp.js
+++ 02.CSS_manipulation/src/cssProp.js
@@ -1,5 +1,32 @@
-function cssProp() {
+var cssProp = function(element, keyOrMap, valueIfPresent) {
   'use strict';
+  requireElementToBePresent(element);
+  return Optional.of(valueIfPresent)
+                 .ifPresent(value => element.style[keyOrMap] = value)
+                 .orElseGet(() => parseKeyOrMap(element, keyOrMap));
+};
 
-  //code goes here
+function requireElementToBePresent(element) {
+    if(element == null) {
+        throw "Element must not be null.";
+    }
+}
+
+function parseKeyOrMap(element, keyOrMap) {
+    if(isMap(keyOrMap)) {
+        addCssPropertiesFromMap(element, keyOrMap);
+    } else {
+        var keyIsARequestedCssProperty = keyOrMap;
+        return element.style[keyIsARequestedCssProperty];
+    }
+}
+
+function isMap(keyOrMap) {
+    return keyOrMap instanceof Object;
+}
+
+function addCssPropertiesFromMap(element, map) {
+    for(var keyAsCssProperty in map) {
+        element.style[keyAsCssProperty] = map[keyAsCssProperty];
+    }
 }
